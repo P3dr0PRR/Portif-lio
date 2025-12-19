73681385696831ab95f394c49d4c5858b0582838
Pedro <pedropaulo@interlinksistemas.com.br>
2025-12-18 23:48:49 -0300

    Adicionei a seção Projetos, falta adicionar os projetos de fato

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,11 +1,13 @@
 import { About } from "../Body/About.jsx";
 import { QuemSou } from "../Body/QuemSou.jsx";
+import { Projetos } from "../Body/Projetos.jsx";
 
 export function Body() {
   return (
     <div className="flex flex-col">
       <About />
       <QuemSou />
+      <Projetos />
     </div>
   );
 }
---
+++ src/Body/Projetos.jsx
@@ -0,0 +1,21 @@
+export function Projetos() {
+  return (
+    <div>
+      <h2 className="text-4xl font-bold mb-6 text-white text-center mt-10">
+        Projetos
+      </h2>
+      <div className="flex flex-col md:flex-row w-full items-center justify-center md:justify-between px-6 md:px-20 gap-10">
+        // A adicionar
+      </div>
+      <h3 className="text-lg leading-relaxed text-white text-center mx-10 mt-10">
+        Nestes projetos, pratico diariamente conceitos de HTML, CSS, Tailwind,
+        JavaScript e React, criando componentes, interfaces e funcionalidades
+        reais. São exercícios estruturados para melhorar minha lógica, domínio
+        do DOM, eventos, funções, responsividade e boas práticas de código. Cada
+        projeto representa um passo da minha evolução como desenvolvedor
+        Front-end e reflete minha busca constante por organização, clareza e
+        aprendizado contínuo.
+      </h3>
+    </div>
+  );
+}
