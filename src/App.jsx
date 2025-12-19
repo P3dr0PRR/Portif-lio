7bfcc1dcf9f4e39c37c267caeceb0e9563c189de
Pedro <pedropaulo@interlinksistemas.com.br>
2025-12-19 00:08:28 -0300

    adicionei o footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 
 import { Body } from "./Body/Body.jsx";
+import { Footer } from "./Footer/footer.jsx";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-black">
       <Body />
+      <Footer />
     </div>
   );
 };
---
+++ src/Footer/footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="bg-stone-600 text-white pb-1 text-center">
+      <p>&copy; 2025 Pedro Paulo. Todos os direitos reservados.</p>
+    </footer>
+  );
+}
